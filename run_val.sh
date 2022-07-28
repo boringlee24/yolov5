dddf1531b39651127a7c2a02fa29de298e21ae45
Baolin Li <baolin.li1994@gmail.com>
2022-07-28 10:54:53 -0700

    conf level

--- run_val.sh
+++ run_val.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python val.py --weights yolov5x.pt --data coco.yaml --img 640 --half --conf-thres 0.5
+for CONF in 0.6 0.7 0.8
+do 
+	echo running yolov5x confidence $CONF
+	python val.py --weights yolov5x.pt --data coco.yaml --img 640 --half --conf-thres $CONF > runs/exp_v5x_${CONF}conf.log &&
+	sleep 10 
+	echo running yolov5s confidence $CONF
+	python val.py --weights yolov5s.pt --data coco.yaml --img 640 --half --conf-thres $CONF > runs/exp_v5s_${CONF}conf.log &&
+	sleep 10
+done
+
