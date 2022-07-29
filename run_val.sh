29496248284b359657d8d403b68f8630919f6574
Baolin Li <baolin.li1994@gmail.com>
2022-07-29 14:46:54 -0700

    v5x6 1280p working

--- run_val.sh
+++ run_val.sh
@@ -3,10 +3,14 @@
 for CONF in 0.01 0.5 0.6 0.7 0.8
 do 
 	echo running yolov5x confidence $CONF
-	python val.py --weights yolov5x.pt --data coco.yaml --img 1280 --half --conf-thres $CONF > runs/exp_v5x_1280p_${CONF}conf.log &&
+	python val.py --weights yolov5x6.pt --data coco.yaml --img 1280 --half --conf-thres $CONF > runs/exp_v5x6_1280p_${CONF}conf.log &&
 	sleep 10 
 	# echo running yolov5s confidence $CONF
 	# python val.py --weights yolov5s.pt --data coco.yaml --img 640 --half --conf-thres $CONF > runs/exp_v5s_${CONF}conf.log &&
 	# sleep 10
 done
+
+echo running yolov5x 640p 0.5conf
+
+python val.py --weights yolov5x.pt --data coco.yaml --img 640 --half --conf-thres 0.5
 
